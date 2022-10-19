af79bd9f3678f3fe1b0f4bead1af9962577c845a
Connor Howington <connor.howington@gmail.com>
2022-10-18 20:37:17 -0400

    Add clue selection logic

--- examples/react-web/src/tic-tac-toe/board.js
+++ examples/react-web/src/tic-tac-toe/board.js
@@ -8,9 +8,20 @@
 
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import _ from 'lodash';
 import './board.css';
 
-const Card = ({ width, color, number, letter, hasBorder }) => {
+const cardColors = {
+  blue: '#59A1E4',
+  yellow: '#E4BD59',
+  red: '#EB5E5E',
+  green: '#59DE44',
+  white: 'white',
+  purple: '#9F59E4',
+  none: 'none',
+};
+
+const Card = ({ width, color, id, letter, isEmpty, isHighlighted }) => {
   const height = width * 1.6;
 
   return (
@@ -19,16 +30,20 @@ const Card = ({ width, color, number, letter, hasBorder }) => {
         style={{
           width: '100%',
           height: '100%',
-          backgroundColor: color ?? 'violet',
+          backgroundColor: cardColors[color] ?? cardColors['purple'],
           borderRadius: 5,
           position: 'relative',
-          border: hasBorder ? 'dotted black 2px' : undefined,
+          border: isEmpty
+            ? 'dotted black 2px'
+            : isHighlighted
+            ? 'solid orange 4px'
+            : undefined,
           boxSizing: 'border-box',
           padding: 2,
         }}
       >
         <div style={{ fontSize: 16 }}>{letter}</div>
-        <div style={{ position: 'absolute', bottom: 0 }}>{number}</div>
+        <div style={{ position: 'absolute', bottom: 0 }}>{id}</div>
       </div>
     </div>
   );
@@ -36,9 +51,7 @@ const Card = ({ width, color, number, letter, hasBorder }) => {
 
 const Hand = ({ cards, width, hidden }) => {
   const cardWidth = width / cards.length;
-  const newCards = hidden
-    ? cards.map((card) => ({ number: card.number }))
-    : cards;
+  const newCards = hidden ? cards.map((card) => ({ id: card.id })) : cards;
 
   return (
     <div style={{ display: 'flex', width }}>
@@ -49,7 +62,7 @@ const Hand = ({ cards, width, hidden }) => {
   );
 };
 
-const ActionButton = ({ text, onClick }) => {
+const ActionButton = ({ text, onClick, disabled }) => {
   return (
     <button
       style={{
@@ -57,15 +70,17 @@ const ActionButton = ({ text, onClick }) => {
         width: 80,
         height: 30,
         border: 'none',
+        opacity: disabled ? 0.7 : 1,
       }}
       onClick={onClick}
+      disabled={disabled}
     >
       {text}
     </button>
   );
 };
 
-const ClueButton = ({ color, text, onClick, isActive }) => {
+const ClueButton = ({ color, text, onClick, isActive, isHighlighted }) => {
   const size = 40;
 
   return (
@@ -74,9 +89,9 @@ const ClueButton = ({ color, text, onClick, isActive }) => {
         width: size,
         height: size,
         borderRadius: size / 2,
-        backgroundColor: color ?? 'violet',
-        border: 'none',
-        opacity: isActive ? 1 : 0.5,
+        backgroundColor: cardColors[color] ?? cardColors['purple'],
+        border: isHighlighted ? 'solid 4px orange' : 'none',
+        opacity: isActive ? 1 : 0.3,
         boxSizing: 'border-box',
       }}
       onClick={onClick}
@@ -87,9 +102,12 @@ const ClueButton = ({ color, text, onClick, isActive }) => {
   );
 };
 
-const ClueModal = ({ playerHand, closeModal }) => {
-  const activeColors = new Set(playerHand.map((card) => card.color));
-  const activeLetters = new Set(playerHand.map((card) => card.letter));
+const ClueModal = ({ playerHand, closeModal, setSelectedCards, isOpen }) => {
+  const [selectedButton, setSelectedButton] = useState(undefined);
+  const colorGroups = _.groupBy(playerHand, (card) => card.color);
+  const letterGroups = _.groupBy(playerHand, (card) => card.letter);
+  const activeColors = Object.keys(colorGroups);
+  const activeLetters = Object.keys(letterGroups);
   const allColors = ['red', 'yellow', 'green', 'blue', 'white'];
   const allLetters = ['A', 'B', 'C', 'D', 'E'];
 
@@ -99,13 +117,17 @@ const ClueModal = ({ playerHand, closeModal }) => {
         width: '100%',
         height: '100%',
         backgroundColor: 'rgba(1, 1, 1, 0.5)',
-        display: 'flex',
+        display: isOpen ? 'flex' : 'none',
         alignItems: 'center',
         justifyContent: 'center',
         position: 'absolute',
         bottom: 0,
       }}
-      onClick={closeModal}
+      onClick={() => {
+        closeModal();
+        setSelectedButton(undefined);
+        setSelectedCards([]);
+      }}
     >
       <div
         style={{
@@ -128,8 +150,12 @@ const ClueModal = ({ playerHand, closeModal }) => {
           {allColors.map((color) => (
             <ClueButton
               color={color}
-              onClick={() => {}}
-              isActive={activeColors.has(color)}
+              onClick={() => {
+                setSelectedButton(color);
+                setSelectedCards(colorGroups[color].map((card) => card.id));
+              }}
+              isActive={activeColors.includes(color)}
+              isHighlighted={selectedButton === color}
             />
           ))}
         </div>
@@ -143,14 +169,22 @@ const ClueModal = ({ playerHand, closeModal }) => {
         >
           {allLetters.map((letter) => (
             <ClueButton
-              onClick={() => {}}
-              isActive={activeLetters.has(letter)}
+              onClick={() => {
+                setSelectedButton(letter);
+                setSelectedCards(letterGroups[letter].map((card) => card.id));
+              }}
+              isActive={activeLetters.includes(letter)}
               text={letter}
+              isHighlighted={selectedButton === letter}
             />
           ))}
         </div>
         <br />
-        <ActionButton text="Send clue" onClick={() => {}} />
+        <ActionButton
+          text="Send clue"
+          onClick={() => {}}
+          disabled={!selectedButton}
+        />
       </div>
     </div>
   );
@@ -158,32 +192,43 @@ const ClueModal = ({ playerHand, closeModal }) => {
 
 const Screen = ({ playerID, playerTurn }) => {
   const [clueModalOpen, setClueModalOpen] = useState(false);
+  const [selectedCards, setSelectedCards] = useState([]);
+  console.log({ selectedCards });
 
   const width = 300;
   const height = 600;
 
   const p0Cards = [
-    { color: 'yellow', number: 1, letter: 'A' },
-    { color: 'blue', number: 2, letter: 'B' },
-    { color: 'blue', number: 3, letter: 'C' },
-    { color: 'white', number: 4, letter: 'D' },
-    { color: 'green', number: 5, letter: 'E' },
+    { color: 'yellow', id: 1, letter: 'A' },
+    { color: 'blue', id: 2, letter: 'B' },
+    { color: 'blue', id: 3, letter: 'C' },
+    { color: 'white', id: 4, letter: 'D' },
+    { color: 'green', id: 5, letter: 'E' },
   ];
 
   const p1Cards = [
-    { color: 'red', number: 6, letter: 'D' },
-    { color: 'blue', number: 7, letter: 'D' },
-    { color: 'green', number: 8, letter: 'A' },
-    { color: 'yellow', number: 9, letter: 'E' },
-    { color: 'white', number: 10, letter: 'C' },
+    { color: 'red', id: 6, letter: 'D' },
+    { color: 'blue', id: 7, letter: 'D' },
+    { color: 'green', id: 8, letter: 'A' },
+    { color: 'yellow', id: 9, letter: 'E' },
+    { color: 'white', id: 10, letter: 'C' },
   ];
+
+  const currentPlayerCards = playerID === '0' ? p0Cards : p1Cards;
+  const otherPlayerCards = (playerID === '0' ? p1Cards : p0Cards).map(
+    (card) => {
+      return selectedCards.includes(card.id)
+        ? { isHighlighted: true, ...card }
+        : card;
+    }
+  );
 
   const boardCards = [
     { color: 'green', letter: 'A' },
     { color: 'white', letter: 'B' },
     { color: 'yellow', letter: 'C' },
     { color: 'red', letter: 'D' },
-    { letter: 'Blue', hasBorder: true, color: 'none' },
+    { letter: 'Blue', isEmpty: true, color: 'none' },
   ];
 
   return (
@@ -200,7 +245,7 @@ const Screen = ({ playerID, playerTurn }) => {
       }}
     >
       <div>
-        <Hand width={width} cards={playerID === '0' ? p1Cards : p0Cards} />
+        <Hand width={width} cards={otherPlayerCards} />
       </div>
       <div style={{ flex: '1 0 auto', display: 'flex', position: 'relative' }}>
         <div
@@ -232,19 +277,15 @@ const Screen = ({ playerID, playerTurn }) => {
             <Hand width={width * 0.8} cards={boardCards} />
           </div>
           <div>
-            <Hand
-              width={width}
-              cards={playerID === '0' ? p0Cards : p1Cards}
-              hidden
-            />
+            <Hand width={width} cards={currentPlayerCards} hidden />
           </div>
         </div>
-        {clueModalOpen && (
-          <ClueModal
-            playerHand={playerID === '0' ? p1Cards : p0Cards}
-            closeModal={() => setClueModalOpen(false)}
-          />
-        )}
+        <ClueModal
+          playerHand={otherPlayerCards}
+          closeModal={() => setClueModalOpen(false)}
+          setSelectedCards={setSelectedCards}
+          isOpen={clueModalOpen}
+        />
       </div>
     </div>
   );
